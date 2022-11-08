4ee483371f698b7129dfb09289741883eb74efd5
Armando Garcia <armando.garcia@ottersoft.com>
2022-11-07 19:38:46 -0800

    Chore; prod

--- routes/hokey-public.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Router = require("koa-router");
-const hockey_1 = require("../models/hockey");
-const router = new Router();
-router.get('/hokey', hockey_1.Hokey.getLessons);
-router.post('/hokey/book', hockey_1.Hokey.bookLesson);
-exports.default = router.routes();
